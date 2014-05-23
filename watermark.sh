06d8a9d3f4c23c1a2f5081af9b84f102b8ac94d6
Chen Wei <acee06.weichen@gmail.com>
2014-05-23 10:48:19 +0800

    rescale image size

--- watermark.sh
+++ watermark.sh
@@ -8,7 +8,7 @@ cd ${current}/${srcdir}
 for src in *.JPG
 do
     echo $src
-    convert -quality 80 -scale 70% $src \
+    convert -quality 75 -scale 50% $src \
         -gravity SouthEast -font Courier-bold -fill SlateBlue1 \
         -pointsize 48 -annotate +50+50 'chenweiyj.github.io' ${current}/${destdir}/$src
 done
