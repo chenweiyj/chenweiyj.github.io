a817f50be7aa5ba60e12913b65b6e7c38d888107
Chen Wei <acee06.weichen@gmail.com>
2014-05-23 11:03:59 +0800

    rearrage watermark

--- watermark.sh
+++ watermark.sh
@@ -3,13 +3,16 @@
 current=`pwd`
 srcdir=imageo
 destdir=image
+watermark=chenweiyj.github.io
 
 cd ${current}/${srcdir}
 for src in *.JPG
 do
+    # scale: 80% pointsize: 48 annotate: +50+50
+    # scale: 50% pointsize: 32 annotate: +30+30
     echo $src
     convert -quality 75 -scale 50% $src \
         -gravity SouthEast -font Courier-bold -fill SlateBlue1 \
-        -pointsize 48 -annotate +50+50 'chenweiyj.github.io' ${current}/${destdir}/$src
+        -pointsize 32 -annotate +30+30 "${watermark}" ${current}/${destdir}/$src
 done
 cd ${current}
